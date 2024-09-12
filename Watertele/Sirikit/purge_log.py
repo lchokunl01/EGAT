ae2fa8ce2f1928478ca20ab227547e798cc99301
lchokunl01 <155959379+lchokunl01@users.noreply.github.com>
2024-09-12 16:06:41 +0700

    update purge_log.py

--- Watertele/Sirikit/purge_log.py
+++ Watertele/Sirikit/purge_log.py
@@ -17,5 +17,11 @@ fname = last_hour_date_time.strftime('%Y-%m-%d.log')
 
 print fname
 
+os.system('sudo rm /var/log/rainT_' + fname)
+
+fname = last_hour_date_time.strftime('%Y-%m-%d.log')
+
+print fname
+
 os.system('sudo rm /var/log/water_' + fname)
 
